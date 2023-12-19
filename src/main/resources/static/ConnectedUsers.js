6bf9efe423f72f6026d2ffbd05042833860435ce
aldeano <oscaral566@gmail.com>
2023-12-19 19:04:21 +0100

    usuarios conectados

---
+++ src/main/resources/static/ConnectedUsers.js
@@ -0,0 +1,33 @@
+var userid;
+
+$(document).ready(function() {
+    $.ajax({
+        type: 'POST',
+        url: `http://${ipAddress}:8080/User/connect`,
+        success: function(response) {
+            userid = response;
+            console.log("Usuario conectado");
+        },
+        error: function(xhr, status, error) {
+            console.log("Error en el POST conectar Usuario");
+            console.log(error);
+        }
+    });
+    setInterval(obtenerUsuariosConectados, 100); 
+});
+
+function obtenerUsuariosConectados() {
+    $.ajax({
+        url: `http://${ipAddress}:8080/User/aliveUsers`,
+        type: 'GET',
+        data: {userId : userid},
+        success: function(data) {
+            $('#connectedUsersCount').text('Usuarios conectados: ' + data);
+        },
+        error: function(xhr, status, error) {
+            console.error('Error al obtener el número de usuarios conectados:', error);
+        }
+    });
+}
+
+
