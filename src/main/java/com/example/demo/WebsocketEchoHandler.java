404e090db47dfa1515b3c2f8a746af9d0aac2c00
Ndj-13 <nerea.dzjrc@gmail.com>
2023-12-30 12:06:56 +0100

    Envio de mensajes en JSON

--- src/main/java/com/example/demo/WebsocketEchoHandler.java
+++ src/main/java/com/example/demo/WebsocketEchoHandler.java
@@ -1,15 +1,53 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 public class WebsocketEchoHandler extends TextWebSocketHandler {
-    @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        System.out.println("Message received: " + message.getPayload());
-        String msg = message.getPayload();
-        session.sendMessage(new TextMessage(msg));
-    }
+
+    private Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+	private int maxOnline = 2;
+	private ObjectMapper mapper = new ObjectMapper();
+
+    //Notify a connection
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception 
+	{
+		System.out.println("New session" + session.getId());
+		sessions.put(session.getId(), session);
+	}
+	//Notify a disconnection 
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception 
+	{
+		System.out.println("Session closed: " + session.getId());
+		sessions.remove(session.getId());
+	}
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception 
+	{
+		JsonNode node = mapper.readTree(message.getPayload());
+		sendOtherParticipants(session, node);
+	}
+	
+	private void sendOtherParticipants(WebSocketSession session, JsonNode node) throws IOException 
+	{
+		ObjectNode newNode = mapper.createObjectNode();
+        if(node.get("id")!= null) 
+		{
+				newNode.put("id", node.get("id").asInt());
+		}
+		
+	}
 
 }
